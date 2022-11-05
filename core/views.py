0222da12fad4da76e1f7bbbb79573cda70de6ed5
tgod <tonmeje@gmail.com>
2022-11-05 13:32:15 +0100

    initail commit

---
+++ core/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .models import Profile
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='signin')
+def index(request):
+    return render(request, 'index.html')
+
+
+@login_required(login_url='signin')
+def setting(request):
+    user_profile = Profile.objects.get(user=request.user)
+    
+    return render(request, 'setting.html', {'user_profile':user_profile})
+
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exist')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exist')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password, email=email)
+                user.save()
+
+                login_user = auth.authenticate(username=username, password=password)
+                auth.login(request, login_user)
+
+                user_model = User.objects.get(username=username)
+                profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id)
+                profile.save()
+                return redirect('setting')
+        else:
+            messages.info(request, 'Password does not match')
+            return redirect('signup')
+
+    else:
+        return render(request, 'signup.html')
+
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Inavlid Username or Password')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
