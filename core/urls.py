1ee01500c8b51e34f41bb12e4d574b94a14a1afa
tgod <tonmeje@gmail.com>
2022-11-09 23:53:25 +0100

    profile section

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('setting', views.setting, name='setting'),
     path('upload', views.upload, name='upload'),
     path('like-post', views.like_post, name='like-post'),
+    path('profile/<str:pk>', views.profile, name='profile'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
