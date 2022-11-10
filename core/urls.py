906fbd0c2fd57ed17b3401a0cc7042dec78247d8
tgod <tonmeje@gmail.com>
2022-11-10 15:08:41 +0100

    follwers and following

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('setting', views.setting, name='setting'),
     path('upload', views.upload, name='upload'),
+    path('follow', views.follow, name='follow'),
     path('like-post', views.like_post, name='like-post'),
     path('profile/<str:pk>', views.profile, name='profile'),
     path('signup', views.signup, name='signup'),
